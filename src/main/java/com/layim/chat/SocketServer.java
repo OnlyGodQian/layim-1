7623104b799dfdd8d96a9a4bd56c6d8868c236a3
shensk <164463450@qq.com>
2016-10-14 21:59:12 +0800

    add demo

---
+++ src/main/java/com/layim/chat/SocketServer.java
@@ -0,0 +1,44 @@
+package com.layim.chat;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/SocketServer")
+public class SocketServer {
+    @OnOpen
+    public void onOpen(Session session){
+        System.out.println(1);
+    }
+
+    /**
+     * 连接关闭调用的方法
+     */
+    @OnClose
+    public void onClose(){
+        
+    }
+
+    /**
+     * 收到客户端消息后调用的方法
+     * @param message 客户端发送过来的消息
+     * @param session 可选的参数
+     */
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        
+    }
+
+    /**
+     * 发生错误时调用
+     * @param session
+     * @param error
+     */
+    @OnError
+    public void onError(Session session, Throwable error){
+        
+    }
+}
